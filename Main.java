c88bbe3535a59609df4ec800520e33c0bb8ad239
VijayTeja-10 <vijayteja8000@gmail.com>
2025-08-15 15:30:51 +0530

    First Commit

---
+++ Main.java
@@ -0,0 +1,57 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        System.err.print("Enter your name : ");
+        Scanner in=new Scanner(System.in);
+        String name=in.next();
+        System.out.println("Hello, "+name);
+        boolean run=true;
+        while (run) {
+            int wrk = choice();
+            if (wrk <= 3) {
+                if (wrk == 1) {
+                    bets();
+
+                } else if (wrk==2) {
+                    play(name);
+                }else{
+                    run=false;
+                }
+            }else {
+                System.err.println("Enter a valid Choice!");
+            }
+        }
+    }
+    public static void bets(){
+        System.out.println("bets    wins");
+        System.out.print("1$   - 10$\n10$  - 100$\n100$ - 1000$\n");
+    }
+    public static void play(String name){
+        Scanner in=new Scanner(System.in);
+        System.out.print("Place Your Bet : ");
+        int bet=in.nextInt();
+        Random rand=new Random();
+        int luck= rand.nextInt(8);
+
+        System.out.println(luck+" "+luck+" "+luck);
+        if((luck*3)==21){
+            System.out.println(name+" You Won "+(bet*10)+"$");
+        }else{
+            System.out.println("Better Luck NextTime");
+        }
+    }
+    static int choice(){
+        System.out.println("bets and info : 1");
+        System.out.println("Play : 2");
+        System.out.println("Exit : 3");
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter Choice : ");
+        try {
+            int ch=in.nextInt();
+            return ch;
+        } catch (Exception e) {
+            System.out.println("Enter a valid choice!");
+        }        
+        return 0;
+    }
+}
